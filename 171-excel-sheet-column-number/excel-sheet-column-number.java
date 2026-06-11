01f65d5ccc6e0635ea7482151b5158e06c90ad32
Vishnu I S <vishnuirappasangammanavar@gmail.com>
2026-06-11 10:50:12 +0530

    Time: 1 ms (91.52%) | Memory: 43.5 MB (53.92%) - LeetSync

--- 171-excel-sheet-column-number/excel-sheet-column-number.java
+++ 171-excel-sheet-column-number/excel-sheet-column-number.java
@@ -1,18 +1,11 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
+        int sum = 0;
 
-        HashMap<Character,Integer> dict=new HashMap<>();
+        for(int i = 0; i < columnTitle.length(); i++) {
+            sum = sum * 26 + (columnTitle.charAt(i) - 'A' + 1);
+        }
 
-        for(char ch='A';ch<='Z';ch++){
-            dict.put(ch,ch-'A'+1);
-        }
-        int sum=0;
-        for(int i=0;i<columnTitle.length();i++){
-            char chs=columnTitle.charAt(i);
-            sum=sum*26+dict.get(chs);
-        }
         return sum;
-
-
     }
 }
