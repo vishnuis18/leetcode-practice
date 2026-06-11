b528b85cacfac30de291dca8b96a76cac737d050
Vishnu I S <vishnuirappasangammanavar@gmail.com>
2026-06-11 09:36:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (22.99%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n=digits.length;
-        int last=digits[n-1];
+        
         for(int i=n-1;i>=0;i--){
             if(digits[i]<9){
                 digits[i]+=1;
