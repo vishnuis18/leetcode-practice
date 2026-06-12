d4bfff43901bd0aea6ee9274e01d910bda327e28
Vishnu I S <vishnuirappasangammanavar@gmail.com>
2026-06-13 00:21:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (82.96%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.java
+++ 319-bulb-switcher/bulb-switcher.java
@@ -1,13 +1,5 @@
 class Solution {
     public int bulbSwitch(int n) {
-        int count=0;
-        for(int i=1;i<=n;i++){
-            int root = (int)Math.sqrt(i);
-            if(root*root==i){
-                count++;
-            }
-        }
-        return count;
-        
+        return (int) Math.sqrt(n);
     }
 }
