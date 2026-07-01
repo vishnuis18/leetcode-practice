2a5aaa85fd706261e025fba15fa4f4e98b3fc747
Vishnu I S <vishnuirappasangammanavar@gmail.com>
2026-07-01 23:58:25 +0530

    Time: 15 ms (61.63%) | Memory: 51.6 MB (37.07%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0; // child pointer
+        int j = 0; // cookie pointer
+        int count = 0;
+
+        while (i < g.length && j < s.length) {
+
+            if (s[j] >= g[i]) {
+                count++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return count;
+    }
+}
