83d45c3a2964af15c7cce2dd0993d764709ed164
Vishnu I S <vishnuirappasangammanavar@gmail.com>
2026-07-12 12:46:00 +0530

    Time: 1 ms (99.97%) | Memory: 47.2 MB (13.85%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -2,14 +2,10 @@ import java.util.Arrays;
 
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 1; i += 2) {
-            if (nums[i] != nums[i + 1]) {
-                return nums[i];
-            }
+        int ans=0;
+        for(int n:nums){
+            ans=ans^n;
         }
-
-        return nums[nums.length - 1];
+        return ans;
     }
 }
