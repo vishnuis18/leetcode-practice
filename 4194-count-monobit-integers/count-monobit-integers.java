7b1528cf08f38a9c778456ec7952eb2486ff559e
Vishnu I S <vishnuirappasangammanavar@gmail.com>
2026-07-14 00:32:03 +0530

    Time: 17 ms (11.94%) | Memory: 46.5 MB (12.50%) - LeetSync

--- 4194-count-monobit-integers/count-monobit-integers.java
+++ 4194-count-monobit-integers/count-monobit-integers.java
@@ -1,33 +1,31 @@
 class Solution {
     public int countMonobit(int n) {
         
-        String[] str = new String[n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            str[i] = Integer.toBinaryString(i);
+        String[] str=new String[n+1];
+        for(int i=0;i<=n;i++){
+            str[i]=Integer.toBinaryString(i);
         }
-
-        int count = 0;
-
-        for (int j = 0; j <= n; j++) {
-            for (int k = 0; k < str[j].length(); k++) {
-
-                if (str[j].length() == 1) {
+        int count=0;
+        for(int j=0;j<=n;j++){
+            for(int k=0;k<str[j].length();k++){
+                if(str[j].length()==1){
                     count++;
                     break;
                 }
-
-                if (str[j].length() >= 2 && str[j].charAt(k) == '1') {
-                    if (k == str[j].length() - 1) {
+                if(str[j].length()>=2 && str[j].charAt(k)=='1'){
+                    if(k==str[j].length()-1){
                         count++;
                     }
                     continue;
-                } else {
+                }
+                else{
                     break;
                 }
+                
+                
             }
+            
         }
-
         return count;
     }
 }
