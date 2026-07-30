bfa037767dd8c7451e393e4c728edb669a85ce91
Vishnu Irappa Sangammanavar <vishnu.23bce10569@vitbhopal.ac.in>
2026-07-30 17:57:25 +0530

    Time: 3 ms (11.24%) | Memory: 43.2 MB (47.13%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,4 +1,30 @@
+import java.util.Arrays;
+
 class Solution {
+    public int minimumPushes(String word) {
+
+        Integer[] freq = new Integer[26];
+        Arrays.fill(freq, 0);
+
+        for (char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        Arrays.sort(freq, java.util.Collections.reverseOrder());
+
+        int result = 0;
+
+        for (int i = 0; i < 26; i++) {
+            int press = i / 8 + 1;
+            result += press * freq[i];
+        }
+
+        return result;
+    }
+}
+
+
+/*class Solution {
     public int minimumPushes(String word) {
 
         HashMap<Integer, Integer> map = new HashMap<>();
@@ -21,6 +47,7 @@ class Solution {
 
     }
 }
+*/
 /*
 class Solution {
     public int minimumPushes(String word) {
