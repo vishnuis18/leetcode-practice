32526301f27090f72b7d76ae6a045e62d27210a2
Vishnu Irappa Sangammanavar <vishnu.23bce10569@vitbhopal.ac.in>
2026-08-06 15:28:18 +0530

    Time: 0 ms (100.00%) | Memory: 47.6 MB (78.85%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,18 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-
-        long binForm = n;
-        if (binForm < 0) {
-            x = 1 / x;
-            binForm = -binForm;
+        long binaryForm=n;
+        if(binaryForm<0){
+            x=1/x;
+            binaryForm=-binaryForm;
         }
-        double ans = 1.0;
-        while (binForm > 0) {
-            if (binForm % 2 == 1) {
-                ans *= x;
+
+        double ans=1.0;
+        while(binaryForm>0){
+            if(binaryForm%2==1){
+                ans=ans*x;
             }
-            x *= x;
-            binForm /= 2;
+            x=x*x;
+            binaryForm=(binaryForm) /2;
         }
         return ans;
     }
