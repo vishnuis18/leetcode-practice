ad8ac35d83b16f8f03ab0c4b87f5578b941af9b3
Vishnu Irappa Sangammanavar <vishnu.23bce10569@vitbhopal.ac.in>
2026-08-06 15:50:13 +0530

    Time: 1 ms (99.95%) | Memory: 94.3 MB (86.51%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -4,6 +4,47 @@ class Solution {
         int maxProfit=0;
 
         for(int price : prices){
+            if(price<minPrice){
+                minPrice=price;
+            }
+
+            int profit=price-minPrice;
+            if(profit>maxProfit){
+                maxProfit=profit;
+            }
+        }
+        return maxProfit;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+
+        for(int price : prices){
 
             if(price<minPrice){
                 minPrice=price;
@@ -17,3 +58,7 @@ class Solution {
         return maxProfit;
     }
 }
+
+
+
+*/
