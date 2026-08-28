124fc655d03708dd892cacc449333a29f0f5379b
Vishnu Irappa Sangammanavar <vishnuirappasangammanavar@gmail.com>
2026-08-29 01:42:34 +0530

    Time: 56 ms (29.63%) | Memory: 70.5 MB (56.12%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -8,6 +8,7 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         while (j < n) {
             map.put(fruits[j], map.getOrDefault(fruits[j], 0) + 1);
+
             while (map.size() > k) {
                 int fruit = fruits[i];
                 map.put(fruit, map.get(fruit) - 1);
@@ -17,6 +18,7 @@ class Solution {
                 i++;
             }
             maxlength = Math.max(maxlength, j - i + 1);
+            
             j++;
         }
         return maxlength;
