3a8d5a603a36dcfe2a11bb61c7d417ce61435500
Vishnu Irappa Sangammanavar <vishnuirappasangammanavar@gmail.com>
2026-09-04 23:18:48 +0530

    Time: 2 ms (73.20%) | Memory: 43.1 MB (12.59%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        if (s.length() == 0) {
+            return true;
+        }
+
+        if (t.length() == 0) {
+            return false;
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+
+            j++;
+        }
+        return i == s.length();
+    }
+}
