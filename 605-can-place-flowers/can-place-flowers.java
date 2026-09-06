a2a4482195bc61d3f546482c8396c57f47d1057a
Vishnu Irappa Sangammanavar <vishnuirappasangammanavar@gmail.com>
2026-09-06 15:42:07 +0530

    Time: 3 ms (2.53%) | Memory: 47.1 MB (99.31%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int s=flowerbed.length;
+        ArrayList<Integer> sample=new ArrayList<>();
+        for(int num :flowerbed){
+            sample.add(num);
+        }
+        if(n==0){
+            return true;
+        }
+
+        for(int i=0;i<s;i++){
+            if( sample.get(i)==0 && (i==0||sample.get(i-1)==0) && (i==s-1|| sample.get(i+1)==0)){
+                sample.set(i,1);
+                n--;
+            }
+            if(n==0){
+                return true;
+            }
+        
+        }
+        
+        return false;
+    }
+}
