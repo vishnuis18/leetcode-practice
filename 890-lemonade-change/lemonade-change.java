30f6598d3cf606fb29c95441157e4bb218ed9bc6
Vishnu Irappa Sangammanavar <vishnuirappasangammanavar@gmail.com>
2026-09-06 17:23:27 +0530

    Time: 16 ms (4.99%) | Memory: 82 MB (5.84%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,25 +1,29 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int a=0,b=0,c=0;
-        
-        for(int i=0;i<bills.length;i++){
-            if(bills[i]==5){
-                a++;
-            }else if(bills[i]==10){
-                if(a!=0){
-                    b++;
-                    a--;
-                }else{
+        int n=bills.length;
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        map.put(5,0);
+        map.put(10,0);
+        map.put(20,0);
+        for(int num: bills){
+            if(num==5){
+                map.put(num,map.get(num)+1);
+            }
+            else if(num==10){
+                map.put(num,map.get(num)+1);
+                if(map.get(5)==0){
                     return false;
+                }else{
+                    map.put(5,map.get(5)-1);
                 }
             }else{
-                if(b>=1 && a>=1){
-                    c++;
-                    a--;
-                    b--;
-                }else if(a>=3){
-                    c++;
-                    a=a-3;
+                map.put(num,map.get(num)+1);
+                if(map.get(10)>=1 && map.get(5)>=1){
+                    map.put(10,map.get(10)-1);
+                    map.put(5,map.get(5)-1);
+                }else if(map.get(5)>=3){
+                    map.put(5,map.get(5)-3);
                 }else{
                     return false;
                 }
