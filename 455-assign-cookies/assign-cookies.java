3f873fd6e29795810eb2112dbc9133446953d0f2
Vishnu Irappa Sangammanavar <vishnuirappasangammanavar@gmail.com>
2026-09-06 17:54:22 +0530

    Time: 15 ms (60.01%) | Memory: 51.8 MB (6.48%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,3 +1,41 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int count=0;
+        int i=0;
+        int j=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(i<g.length && j<s.length){
+            if(g[i]<=s[j]){
+                count++;
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return count;
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import java.util.Arrays;
 
 class Solution {
@@ -6,8 +44,8 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
 
-        int i = 0; // child pointer
-        int j = 0; // cookie pointer
+        int i = 0; 
+        int j = 0; 
         int count = 0;
 
         while (i < g.length && j < s.length) {
@@ -23,4 +61,4 @@ class Solution {
 
         return count;
     }
-}
+}*/
