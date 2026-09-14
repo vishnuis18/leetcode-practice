a846711e1d43069af3f95fd8f4f93c4400f91f73
Vishnu Irappa Sangammanavar <vishnuirappasangammanavar@gmail.com>
2026-09-15 01:11:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (53.10%) - LeetSync

--- 866-rectangle-overlap/rectangle-overlap.java
+++ 866-rectangle-overlap/rectangle-overlap.java
@@ -1,5 +1,14 @@
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int width=Math.min(rec1[2],rec2[2])-Math.max(rec1[0],rec2[0]);
+        int length=Math.min(rec1[3],rec2[3])-Math.max(rec1[1],rec2[1]);
+        return width>0 && length>0;
+    }
+}
+
+/*
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
         if (rec1[0] >= rec2[2] || rec2[0] >= rec1[2]){
             return false;
         }
@@ -9,3 +18,4 @@ class Solution {
         return true;
     }
 }
+*/
