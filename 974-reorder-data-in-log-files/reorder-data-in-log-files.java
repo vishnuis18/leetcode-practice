00f19c569af519046ab46ebd97a4bb410d92bec8
Vishnu Irappa Sangammanavar <vishnuirappasangammanavar@gmail.com>
2026-09-15 14:16:47 +0530

    Time: 7 ms (24.39%) | Memory: 46.5 MB (67.68%) - LeetSync

--- 974-reorder-data-in-log-files/reorder-data-in-log-files.java
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.java
@@ -4,35 +4,32 @@ class Solution {
 
     public String[] reorderLogFiles(String[] logs) {
 
-        Arrays.sort(logs, (a, b) -> {
+        Arrays.sort(logs,(a,b) -> {
+            String[] x=a.split(" ",2);
+            String[] y=b.split(" ",2);
 
-            String[] x = a.split(" ", 2);
-            String[] y = b.split(" ", 2);
+            boolean xDigit=Character.isDigit(x[1].charAt(0));
+            boolean yDigit=Character.isDigit(y[1].charAt(0));
 
-            boolean xDigit = Character.isDigit(x[1].charAt(0));
-            boolean yDigit = Character.isDigit(y[1].charAt(0));
-
-            if (xDigit && yDigit) {
+            if(xDigit && yDigit){
                 return 0;
             }
 
-            if (xDigit) {
+            if(xDigit){
                 return 1;
             }
 
-            if (yDigit) {
+            if(yDigit){
                 return -1;
             }
 
-            int result = x[1].compareTo(y[1]);
+            int result=x[1].compareTo(y[1]);
 
-            if (result == 0) {
+            if(result==0){
                 return x[0].compareTo(y[0]);
             }
-
             return result;
         });
-
         return logs;
     }
 }
