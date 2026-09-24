0689fd315577c2827b07cd90840c0b2d456fe92c
Vishnu Irappa Sangammanavar <vishnuirappasangammanavar@gmail.com>
2026-09-24 11:51:31 +0530

    Time: 1 ms (99.69%) | Memory: 45.6 MB (40.74%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -3,12 +3,10 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
             int sum = 0;
-
             while (num > 0) {
                 sum += num % 10;
                 num /= 10;
             }
-
             if (sum == i) {
                 return i;
             }
@@ -16,3 +14,23 @@ class Solution {
         return -1;
     }
 }
+/*
+class Solution {
+    public int smallestIndex(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            int num = nums[i];
+            String no = String.valueOf(num);
+            for (int j = 0; j < no.length(); j++) {
+                char ch = no.charAt(j);
+                sum = sum + ch - '0';
+            }
+            if (sum == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+*/
